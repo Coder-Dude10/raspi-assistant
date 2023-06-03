559bf1b44bb52538b04a169c27940b59f91f8d65
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-06-03 16:19:33 -0500

    Update raspi.js

--- raspi.js
+++ raspi.js
@@ -1,9 +1,11 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var audio = require("play-sound")(opts = {});
 var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log(JSON.parse(this.responseText).properties);
+        audio.play("reverb-fart.mp3");
     }
 };
 xhttp.open("GET", "https://api.weather.gov/gridpoints/TOP/86,72/forecast", true);
