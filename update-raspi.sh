7ef44976dc300a2f0a917e138849828f131e64b5
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-07 19:52:24 -0500

    Update update-raspi.sh

--- update-raspi.sh
+++ update-raspi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm "pacman.mp3"
-rm "raspi.js"
-rm "update-raspi.sh"
-cd "~"
-rmdir "raspi-assistant-main"
+rm pacman.mp3
+rm raspi.js
+rm update-raspi.sh
+cd ~
+rmdir raspi-assistant-main
 wget "https://github.com/Coder-Dude10/raspi-assistant/archive/refs/heads/main.zip"
-sleep "5s"
-unzip "main.zip"
-rm "main.zip"
+sleep 5s
+unzip main.zip
+rm main.zip
 echo "Update complete!"
