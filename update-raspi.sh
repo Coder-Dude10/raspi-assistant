22ff25c37260a44acbe5f7703e569cccd70495fa
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-07 19:32:12 -0500

    Update update-raspi.sh

--- update-raspi.sh
+++ update-raspi.sh
@@ -7,7 +7,7 @@ rm update-raspi.sh
 cd ~
 rmdir raspi-assistant-main
 wget https://github.com/Coder-Dude10/raspi-assistant/archive/refs/heads/main.zip
-sleep 5
+sleep 5s
 unzip main.zip
 rm main.zip
 echo "Update complete!"
