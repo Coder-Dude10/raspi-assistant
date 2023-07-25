22edcf6a4147e2c4e78e86c04bc7c50631e67b1d
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-24 19:23:34 -0500

    Add files via upload

---
+++ blue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'b' > /dev/ttyACM0
+sleep 0.5
+echo 'l' > /dev/ttyACM0
+sleep 0.5
+echo 'u' > /dev/ttyACM0
+sleep 0.5
+echo 'e' > /dev/ttyACM0
+sleep 0.5
+echo ' ' > /dev/ttyACM0
