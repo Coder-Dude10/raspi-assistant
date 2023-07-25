a17ea54f66856a2aac9bd184d9ead18889cdfdf3
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-24 19:50:15 -0500

    Update rgb.sh

--- rgb.sh
+++ rgb.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-:x
+function jumpto
+{
+    label=$1
+    cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
+    eval "$cmd"
+    exit
+}
+start=${1:-"start"}
+
+start:
 echo '1' > /dev/ttyACM0
 sleep 1
 echo '2' > /dev/ttyACM0
 sleep 1
 echo '3' > /dev/ttyACM0
 sleep 1
-goto x
+jumpto $start
