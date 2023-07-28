9b950cdd810968f08e1e301424287e0a54434d98
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-28 17:56:08 -0500

    Update light.js

--- light.js
+++ light.js
@@ -1,8 +1,8 @@
 const { exec } = require('child_process');
-var audio = require('play-sound');
+var player = require('play-sound');
 
 exec("echo '3' > /dev/ttyACM0");
 
-audio.play('pacman.mp3', function(err){
+player.play('pacman.mp3', function(err){
   if (err) throw err
 });
