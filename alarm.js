094b9aef4eeb4848e50ba643a5b1d1a5b25f81d2
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-28 18:23:08 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 var soundplayer = require('sound-player');
-var player = new soundplayer( { filename: "pacman.mp3", player: "mpg123" } );
+var player = new soundplayer( { filename: "Aces.wav" } );
 
 exec("echo '3' > /dev/ttyACM0");
 player.play();
