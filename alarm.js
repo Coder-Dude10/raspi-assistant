edf6a47dbb485f3a6d8066d969013f50450b6c51
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-30 12:27:35 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,17 +1,13 @@
 const { exec } = require('child_process');
 var soundplayer = require('sound-player');
 var player = new soundplayer( { filename: "223_AM.wav" } );
+var date = new Date();
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  exec("echo 'Hello World!'", (stdout) => {
-    console.log(stdout);
-    var dateTimeString = Buffer.from(stdout);
-
-    if (dateTimeString.includes(" 10:20") && dateTimeString.includes("PM")) {
-      exec("echo '3' > /dev/ttyACM0");
-      player.play();
-    }
-  });
+  if (date.getHours() == 12 && date.getMinutes() == 30) {
+    exec("echo '3' > /dev/ttyACM0");
+    player.play();
+  }
 }
