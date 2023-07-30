59a047a0576b0a244e960c73b02e9a8538b03b5d
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-30 16:50:13 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process');
-var soundplayer = require('sound-player');
+const soundplayer = require('sound-player');
+const fetch = require('node-fetch');
 var player = new soundplayer( { filename: "223_AM.wav" } );
 var date = new Date();
 
@@ -9,6 +10,18 @@ function getTime() {
   if (date.getHours() == 5 && date.getMinutes() == 0) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
+
+    fetch('https://httpbin.org/post', { method: 'POST',
+    url: 'https://large-text-to-speech.p.rapidapi.com/tts',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '2d2bd54781msh26b8bc9bc4fc8a0p12cff1jsn0b6b1018fcab',
+      'X-RapidAPI-Host': 'large-text-to-speech.p.rapidapi.com'
+    },
+    data: {
+      text: 'I like bananas.'
+    } }).then(res => res.text())
+    .then(text => console.log(text));
   }
 
   date = new Date();
