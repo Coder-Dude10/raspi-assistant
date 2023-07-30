6ecaf2115210409c17074e40c5e0a2477769835f
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-30 17:19:49 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,27 +1,36 @@
 const { exec } = require('child_process');
 const soundplayer = require('sound-player');
-const fetch = require('node-fetch');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var player = new soundplayer( { filename: "223_AM.wav" } );
+var xhttp = new XMLHttpRequest();
 var date = new Date();
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 5 && date.getMinutes() == 0 || true) {
+  if (date.getHours() == 5 && date.getMinutes() == 0) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
-
-    fetch('https://httpbin.org/post', { method: 'POST',
-    url: 'https://large-text-to-speech.p.rapidapi.com/tts',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '2d2bd54781msh26b8bc9bc4fc8a0p12cff1jsn0b6b1018fcab',
-      'X-RapidAPI-Host': 'large-text-to-speech.p.rapidapi.com'
-    },
-    data: {
-      text: 'I like bananas.'
-    } }).then(res => res.text())
-    .then(text => console.log(text));
+    
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+      }
+    };
+    
+    xhttp.open({
+      method: 'POST',
+      url: 'https://large-text-to-speech.p.rapidapi.com/tts',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '2d2bd54781msh26b8bc9bc4fc8a0p12cff1jsn0b6b1018fcab',
+        'X-RapidAPI-Host': 'large-text-to-speech.p.rapidapi.com'
+      },
+      data: {
+        text: 'A banana is a botanical fruit.'
+      }
+    });
+    xhttp.send();
   }
 
   date = new Date();
