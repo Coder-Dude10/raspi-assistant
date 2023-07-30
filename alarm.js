7b306b3e14665df4ac024d3f57df9af35345954c
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-30 17:38:03 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,36 +1,16 @@
 const { exec } = require('child_process');
 const soundplayer = require('sound-player');
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const TextToSpeech = require('text-to-speech-js');
 var player = new soundplayer( { filename: "223_AM.wav" } );
-var xhttp = new XMLHttpRequest();
 var date = new Date();
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 5 && date.getMinutes() == 0 || true) {
+  if (date.getHours() == 5 && date.getMinutes() == 0) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
-    
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-      }
-    };
-    
-    xhttp.open({
-      method: 'POST',
-      url: 'https://large-text-to-speech.p.rapidapi.com/tts',
-      headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '2d2bd54781msh26b8bc9bc4fc8a0p12cff1jsn0b6b1018fcab',
-        'X-RapidAPI-Host': 'large-text-to-speech.p.rapidapi.com'
-      },
-      data: {
-        text: 'A banana is a botanical fruit.'
-      }
-    });
-    xhttp.send();
+    TextToSpeech.talk("Bananas are great!");
   }
 
   date = new Date();
