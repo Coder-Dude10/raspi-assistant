9ae18fb02cdabcbcd897c836e1a7fdbae4a9e9d8
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-30 23:36:17 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,16 +1,14 @@
 const { exec } = require('child_process');
 const soundplayer = require('sound-player');
-const TextToSpeech = require('text-to-speech-js');
 var player = new soundplayer( { filename: "223_AM.wav" } );
 var date = new Date();
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 5 && date.getMinutes() == 0) {
+  if (date.getHours() == 6 && date.getMinutes() == 30) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
-    TextToSpeech.talk("Bananas are great!");
   }
 
   date = new Date();
