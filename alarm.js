62198f0fe3a7d23e117feb61fe0918f260468c55
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-07-31 00:26:45 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -6,7 +6,9 @@ var date = new Date();
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 24 && date.getMinutes() == 22) {
+  console.log(date.getHours() + ":" + date.getMinutes());
+  
+  if (date.getHours() == 24 && date.getMinutes() == 30) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
   }
