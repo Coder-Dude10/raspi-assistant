7928d574c413e023b9b51c9b204ad611a3f0bd1c
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-08-07 21:52:06 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,14 +1,23 @@
 const { exec } = require('child_process');
 const soundplayer = require('sound-player');
 var player = new soundplayer( { filename: "223_AM.wav" } );
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhttp = new XMLHttpRequest();
 var date = new Date();
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 6 && date.getMinutes() == 30) {
+  if (date.getHours() == 5 && date.getMinutes() == 0) {
     exec("echo '3' > /dev/ttyACM0");
     player.play();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(xhttp.responseText + "");
+      }
+    };
+    xhttp.open("GET", "https://api.weather.gov/gridpoints/TOP/86,72/forecast", true);
+    xhttp.send();
   }
 
   date = new Date();
