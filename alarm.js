f71a58ff40ea28a17b87d23b0bdb4bc1b9500c2b
Coder-Dude10 <106555946+Coder-Dude10@users.noreply.github.com>
2023-08-08 00:30:36 -0500

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -4,14 +4,12 @@ var player = new soundplayer( { filename: "223_AM.wav" } );
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhttp = new XMLHttpRequest();
 var date = new Date();
-var currentlyPlaying = false;
 
 setInterval(getTime, 60000);
 
 function getTime() {
-  if (date.getHours() == 5 && date.getMinutes() == 0 || !(currentlyPlaying)) {
+  if (date.getHours() == 5 && date.getMinutes() == 0) {
     exec("echo '3' > /dev/ttyACM0");
-    currentlyPlaying = true;
     player.play();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
